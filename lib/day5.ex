20cb6c54729aac4642b500bd40f54177c10c0c22
Jose Arnoldo Segura <jarnoldo2809@gmail.com>
2023-12-06 00:54:59 -0600

    Completed Puzzle 1 Day 5 AOC 2023

---
+++ lib/day5.ex
@@ -0,0 +1,86 @@
+defmodule Day5 do
+  @moduledoc """
+  Module that will contain the answers
+  for the day 5 of Advent of Code 2023.
+  """
+
+  @doc """
+  Solution for Day 5 Puzzle 1
+  """
+  @spec puzzle1(String.t()) :: integer()
+  def puzzle1(fileName) do
+    {:ok, contents} = File.read(fileName)
+    parse_maps(contents)
+    |> get_locations()
+  end
+
+  def parse_maps(input) do
+    [seeds | maps] = String.split(input, "\n\n")
+    n_seeds = String.slice(seeds, 7, String.length(seeds))
+      |> String.split(" ")
+      |> Enum.map(fn(seed) -> {number, ""} = Integer.parse(seed); number end)
+    n_maps = Enum.map(maps,
+      fn(map) ->
+        String.split(map, "\n")
+        |> tl
+        |> convert_to_tuples end)
+    {n_seeds, n_maps}
+  end
+
+  def convert_to_tuples(ranges) do
+    Enum.map(ranges,
+      fn(range) ->
+        [de, so, le] = String.split(range, " ")
+        |> Enum.map(
+            fn(index) ->
+              {number, ""} = Integer.parse(index); number
+            end); {so, de, le}
+      end)
+  end
+
+  def get_locations({seeds, maps}) do
+    Enum.map(seeds, fn(seed) -> seed_maps(seed, maps) end)
+    |> Enum.min
+  end
+
+  def seed_maps(seed, []) do
+    seed
+  end
+  def seed_maps(seed, [map | maps]) do
+    n_seed = seed_map(seed, map)
+    seed_maps(n_seed, maps)
+  end
+
+  def seed_map(seed, []) do
+    seed
+  end
+  def seed_map(seed, [{s, d, l} | t]) do
+    case seed >= s and seed <= (s + (l-1)) do
+      true -> (seed - s) + d
+      _ -> seed_map(seed, t)
+    end
+  end
+
+  @doc """
+  Solution for Day 5 Puzzle 2
+  """
+  @spec puzzle2(String.t()) :: {list(), list()}
+  def puzzle2(fileName) do
+    {:ok, contents} = File.read(fileName)
+    parse_maps(contents)
+    |> range_seeds
+  end
+
+  def range_seeds({seeds, maps}) do
+    n_seeds = create_ranges(seeds, [])
+    {n_seeds, maps}
+  end
+
+  def create_ranges([], acc) do
+    acc
+  end
+  def create_ranges([seeds, length | tail], acc) do
+    create_ranges(tail, acc ++ [{seeds, seeds + length - 1}])
+  end
+
+end
---
+++ test/day5_test.exs
@@ -0,0 +1,36 @@
+defmodule Day5_test do
+  use ExUnit.Case
+  doctest Day5
+
+  test "Test the day 5 puzzle1 against the example file" do
+    assert Day5.puzzle1("test/testfiles/day5_test.txt") == 35
+  end
+
+  # test "Test the day 5 puzzle2 against the example file" do
+  #   assert Day5.puzzle2("test/testfiles/day5_test.txt") == 46
+  #   Day5.puzzle2("test/testfiles/day5_input.txt") |> IO.inspect()
+  # end
+
+  test "Seed convertion" do
+    assert Day5.seed_map(79, [{98, 50, 2}, {50, 52, 48}]) === 81
+    assert Day5.seed_map(14, [{98, 50, 2}, {50, 52, 48}]) === 14
+    assert Day5.seed_map(55, [{98, 50, 2}, {50, 52, 48}]) === 57
+    assert Day5.seed_map(13, [{98, 50, 2}, {50, 52, 48}]) === 13
+  end
+
+  test "Seed multi-convertion" do
+    maps = [[{98, 50, 2}, {50, 52, 48}],
+    [{15, 0, 37}, {52, 37, 2}, {0, 39, 15}],
+    [{53, 49, 8}, {11, 0, 42}, {0, 42, 7}, {7, 57, 4}],
+    [{18, 88, 7}, {25, 18, 70}],
+    [{77, 45, 23}, {45, 81, 19}, {64, 68, 13}],
+    [{69, 0, 1}, {0, 1, 69}],
+    [{56, 60, 37}, {93, 56, 4}]]
+    assert Day5.seed_maps(79, maps) === 82
+    assert Day5.seed_maps(14, maps) === 43
+    assert Day5.seed_maps(55, maps) === 86
+    assert Day5.seed_maps(13, maps) === 35
+  end
+
+
+end
