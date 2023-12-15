13911751baa64329eb6b3a4b993fe8b07a4751e2
Jose Arnoldo Segura <jarnoldo2809@gmail.com>
2023-12-14 20:15:56 -0600

    Completed Day 4 AOC 2023 - Refactored

--- lib/day4.ex
+++ lib/day4.ex
@@ -11,7 +11,7 @@ defmodule Day4 do
   def puzzle1(fileName) do
     {:ok, contents} = File.read(fileName)
     parse_cards(contents)
-    |> verify_cards
+    |> Enum.map(& check_numbers(&1))
     |> Enum.sum()
   end
 
@@ -19,24 +19,20 @@ defmodule Day4 do
     String.split(input, "\n")
     |> Enum.map(
         fn(card) ->
-          card_number = String.split(card, ":") |> hd |> String.split(" ") |> tl |> Enum.filter(fn(card) -> card !== "" end) |> hd
+          card_number = String.split(card, ":") |> hd |> String.split(" ") |> tl |> Enum.filter(& &1 !== "") |> hd
           cards = String.split(card, ":") |> tl |> hd |> String.split("|") |> get_cards
           [card_number, cards]
         end)
   end
 
   def get_cards([winning_card, numbers]) do
-    w_card = String.split(winning_card, " ") |> Enum.filter(fn(card) -> card !== "" end)
-    n_card = String.split(numbers, " ") |> Enum.filter(fn(card) -> card !== "" end)
+    w_card = String.split(winning_card, " ") |> Enum.filter(& &1 !== "")
+    n_card = String.split(numbers, " ") |> Enum.filter(& &1 !== "")
     {w_card, n_card}
   end
 
-  def verify_cards(cards) do
-    Enum.map(cards, fn(card) -> check_numbers(card) end)
-  end
-
   def check_numbers([_, {w_card, n_card}]) do
-    filtered = Enum.filter(n_card, fn(number) -> number in w_card end)
+    filtered = Enum.filter(n_card, & &1 in w_card)
     case length(filtered) do
       0 -> 0
       n -> trunc(:math.pow(2, n-1))
@@ -59,17 +55,13 @@ defmodule Day4 do
     Enum.map(cards,
       fn([card_number, {w_card, n_card}]) ->
         {p_number, ""} = Integer.parse(card_number)
-        {p_number, Enum.filter(n_card, fn(number) -> number in w_card end) |> length}
+        {p_number, Enum.filter(n_card, & &1 in w_card) |> length}
       end)
   end
 
-  def get_copies(cards) do
-    length(cards) + length(get_copies(cards, cards, []))
-  end
-  def get_copies([], _original, copies) do
-    copies
-  end
-  def get_copies([{number, points}|tail], original, copies) do
+  defp get_copies(cards), do: length(cards) + length(get_copies(cards, cards, []))
+  defp get_copies([], _original, copies), do: copies
+  defp get_copies([{number, points}|tail], original, copies) do
     init_copies = Enum.slice(original, number, points)
     derived_copies = get_copies(init_copies, original, [])
     get_copies(tail, original, copies ++ List.flatten([init_copies ++ derived_copies]))
