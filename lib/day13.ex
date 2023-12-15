a57d2c325c62f37e89f1a0440bf38a65dca3f912
Jose Arnoldo Segura <jarnoldo2809@gmail.com>
2023-12-15 01:37:29 -0600

    Completed Day 13 AOC 2023 - Need Refactor

--- lib/day13.ex
+++ lib/day13.ex
@@ -104,18 +104,9 @@ defmodule Day13 do
     h = lines |> find_line_2()
     v = t_lines |> find_line_2()
     case {h, v} do
-      {{h_idx1, _h_idx2}, {v_idx1, _v_idx2}} -> {:h, h_idx1}
+      {{h_idx1, _h_idx2}, {v_idx1, _v_idx2}} -> {:v, h_idx1*100 + v_idx1}
       {:not_found, {v_idx1, _v_idx2}} -> {:v, v_idx1}
       {{h_idx1, _h_idx2}, :not_found} -> {:h, h_idx1}
-      _ -> {:v, 0}
-    end
-  end
-
-
-  def lesser(h_idx, v_idx) do
-    cond do
-      h_idx < v_idx ->{:h, h_idx}
-      true -> {:v, v_idx}
     end
   end
 
@@ -126,36 +117,30 @@ defmodule Day13 do
     |> find_line_22(lines)
   end
 
-  defp find_line_22([_ | []], _) do
-    :not_found
-  end
+  defp find_line_22([_ | []], _), do: :not_found
   defp find_line_22([{elems1, idx1}, {elems2, idx2} | tail], lines) do
     cond do
-      compare_lines(elems1, elems2, 0) in [0, 1] and index_can_propagate2(lines, {idx1, idx2}) -> {idx1, idx2}
+      compare_lines(elems1, elems2, 0) and index_can_propagate2(lines, {idx1, idx2}) -> {idx1, idx2}
       true -> find_line_22([{elems2, idx2} | tail], lines)
     end
   end
 
-  def compare_lines([], _, acc) do
-    acc
-  end
+  def compare_lines([], _, _acc), do: true
   def compare_lines([head1 | tail1], [head2 | tail2], acc) do
     cond do
-      head1 === head2 -> compare_lines(tail1, tail2, acc)
-      true -> compare_lines(tail1,tail2, acc + 1)
+      head1 == head2 -> compare_lines(tail1, tail2, acc)
+      acc == 1 -> false
+      true -> compare_lines(tail1, tail2, acc + 1)
     end
   end
 
   @spec index_can_propagate2(list(), {number(),number()}) :: boolean()
-  def index_can_propagate2(lines, {idx1, idx2}) do
-    index_can_propagate2(lines, {idx1-1, idx2-1}, 1, true)
-  end
-  @spec index_can_propagate2(list(), {number(),number()}, number(), boolean()) :: boolean()
-  def index_can_propagate2(lines, {idx1, idx2}, idx, flag) do
+  def index_can_propagate2(lines, {idx1, idx2}), do: index_can_propagate2(lines, {idx1-1, idx2-1}, 1)
+  @spec index_can_propagate2(list(), {number(),number()}, number()) :: boolean()
+  def index_can_propagate2(lines, {idx1, idx2}, idx) do
     cond do
-      idx1 - idx < 0 or idx2 + idx === length(lines) -> true
-      compare_lines(Enum.at(lines, idx1 - idx), Enum.at(lines, idx2 + idx), 0) === 0 -> index_can_propagate2(lines, {idx1, idx2}, idx + 1, flag)
-      compare_lines(Enum.at(lines, idx1 - idx), Enum.at(lines, idx2 + idx), 0) === 1 and flag -> index_can_propagate2(lines, {idx1, idx2}, idx + 1, false)
+      idx1 - idx < 0 or idx2 + idx == length(lines) -> true
+      compare_lines(Enum.at(lines, idx1 - idx), Enum.at(lines, idx2 + idx), 0) -> index_can_propagate2(lines, {idx1, idx2}, idx + 1)
       true -> false
     end
   end
