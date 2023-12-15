d5e0263ea82c7f3cddfbc5039c4c6ed89869c29d
Jose Arnoldo Segura <jarnoldo2809@gmail.com>
2023-12-14 19:53:23 -0600

    Completed Day 1 AOC 2023 - Refactored

--- lib/day1.ex
+++ lib/day1.ex
@@ -7,20 +7,17 @@ defmodule Day1 do
   @doc """
   Solution for Day 1 Puzzle 1
   """
-  @spec puzzle1(String.t()) :: integer()
+  @spec puzzle1(String.t()) :: number()
   def puzzle1(fileName) do
     {:ok, contents} = File.read(fileName)
     parse_lines(contents)
-    |> List.foldr(0,
-        fn(digits, acc) ->
-          total = (hd(digits) * 10) + List.last(digits);
-          acc + total
-        end)
+    |> Enum.map(& (hd(&1) * 10) + List.last(&1))
+    |> Enum.sum
   end
 
   defp parse_lines(lines) do
     String.split(lines, "\n")
-    |> Enum.map(fn(str) -> scan_convert(str) end)
+    |> Enum.map(& scan_convert(&1))
   end
 
   defp scan_convert(str) do
@@ -31,16 +28,17 @@ defmodule Day1 do
   @doc """
   Solution for Day 1 Puzzle 2
   """
-  @spec puzzle2(String.t()) :: list()
+  @spec puzzle2(String.t()) :: number()
   def puzzle2(fileName) do
     {:ok, contents} = File.read(fileName)
     parse_numbers(contents)
-    |> List.foldr(0, fn(digits, acc) -> total = (hd(digits) * 10) + List.last(digits); total+acc end)
+    |> Enum.map(& (hd(&1) *10) + List.last(&1))
+    |> Enum.sum
   end
 
   defp parse_numbers(lines) do
     String.split(lines, "\n")
-    |> Enum.map(fn(str) -> scan_numbers(str) end)
+    |> Enum.map(& scan_numbers(&1))
   end
 
   defp scan_numbers(str) do
@@ -49,36 +47,15 @@ defmodule Day1 do
     [head, tail]
   end
 
-  defp convert_numbers("one") do
-    1
-  end
-  defp convert_numbers("two") do
-    2
-  end
-  defp convert_numbers("three") do
-    3
-  end
-  defp convert_numbers("four") do
-    4
-  end
-  defp convert_numbers("five") do
-    5
-  end
-  defp convert_numbers("six") do
-    6
-  end
-  defp convert_numbers("seven") do
-    7
-  end
-  defp convert_numbers("eight") do
-    8
-  end
-  defp convert_numbers("nine") do
-    9
-  end
-  defp convert_numbers(number) do
-    {n, ""} = Integer.parse(number)
-    n
-  end
+  defp convert_numbers("one"), do: 1
+  defp convert_numbers("two"), do: 2
+  defp convert_numbers("three"), do: 3
+  defp convert_numbers("four"), do: 4
+  defp convert_numbers("five"), do: 5
+  defp convert_numbers("six"), do: 6
+  defp convert_numbers("seven"), do: 7
+  defp convert_numbers("eight"), do: 8
+  defp convert_numbers("nine"), do: 9
+  defp convert_numbers(number), do: ({n, ""} = Integer.parse(number); n)
 
 end
