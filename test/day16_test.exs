399894a6efc478daf5a60e41266ad5813520a5a8
Jose Arnoldo Segura <jarnoldo2809@gmail.com>
2024-01-10 23:51:23 -0600

    Day 16 AOC 2023 - Fixing tests

--- test/day16_test.exs
+++ test/day16_test.exs
@@ -4,9 +4,8 @@ defmodule Day16_test do
 
   test "Test the day 16 against the example file" do
     #assert Day16.puzzle1("test/testfiles/day16_test.txt") == 46
-    Day16.puzzle1("test/testfiles/day16_input.txt") |> Day16.count_symbols |> IO.inspect
-    File.write("test/testfiles/day16_output.txt", Day16.puzzle1("test/testfiles/day16_input.txt") |> Day16.stringify)
-
+    #Day16.puzzle1("test/testfiles/day16_input.txt") |> Day16.count_symbols |> IO.inspect
+    #File.write("test/testfiles/day16_output.txt", Day16.puzzle1("test/testfiles/day16_input.txt") |> Day16.stringify)
   end
 
   test "energize a cell" do
@@ -37,9 +36,4 @@ defmodule Day16_test do
     assert Day16.energize(cavern, 0, 0, ".") == solution_1
   end
 
-  test "Is point traversed" do
-    closed = [{0, 0, :right}, {0, 1, :right}, {0, 2, :right}, {1, 2, :down}]
-    assert Day16.is_traversed(closed, {0, 2, :right})
-    assert not Day16.is_traversed(closed, {0, 2, :down})
-  end
 end
